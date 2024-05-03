a4121393a911be9dc41a79e035462004665b5425
Roman Timoshkin <rmntim@yandex.ru>
2024-05-03 15:32:22 +0300

    feat: add rooms table

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -67,3 +67,12 @@ export const verificationTokens = pgTable(
     compoundKey: primaryKey({ columns: [vt.identifier, vt.token] }),
   }),
 );
+
+export const room = pgTable("room", {
+  userId: text("userId")
+    .notNull()
+    .references(() => users.id, { onDelete: "cascade" }),
+  name: text("name").notNull(),
+  language: text("language").notNull(),
+  githubRepo: text("githubRepo"),
+});

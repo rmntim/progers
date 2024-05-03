b329d7fff6a5fbdf3299dabafb33e79f5e08a11a
Roman Timoshkin <rmntim@yandex.ru>
2024-05-03 16:17:55 +0300

    feat: add create room form

---
+++ src/app/create-room/create-room-form.tsx
@@ -0,0 +1,130 @@
+"use client";
+
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "~/components/ui/form";
+import { Input } from "~/components/ui/input";
+import { Button } from "~/components/ui/button";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "~/components/ui/select";
+
+//! FIXME: Pull language list form backend
+const languages = ["javascript", "typescript", "python", "go", "rust"] as const;
+
+const formSchema = z.object({
+  name: z.string().min(1).max(50),
+  description: z.string().optional(),
+  language: z.enum(languages),
+  repository: z.string().url().optional(),
+});
+
+export function CreateRoomForm() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+    },
+  });
+
+  const onSubmit = (values: z.infer<typeof formSchema>) => {
+    console.log(values);
+  };
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="flex flex-col gap-4"
+      >
+        <FormField
+          control={form.control}
+          name="name"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Name</FormLabel>
+              <FormControl>
+                <Input placeholder="Enter a name" {...field} />
+              </FormControl>
+              <FormDescription>Public name of the room.</FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="description"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Description</FormLabel>
+              <FormControl>
+                <Input placeholder="Enter a description" {...field} />
+              </FormControl>
+              <FormDescription>
+                Please describe what you will be working on.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="language"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Language</FormLabel>
+              <Select onValueChange={field.onChange} defaultValue={field.value}>
+                <FormControl>
+                  <SelectTrigger>
+                    <SelectValue placeholder="Choose a language" />
+                  </SelectTrigger>
+                </FormControl>
+                <SelectContent>
+                  {languages.map((language) => (
+                    <SelectItem key={language} value={language}>
+                      {language}
+                    </SelectItem>
+                  ))}
+                </SelectContent>
+              </Select>
+              <FormDescription>
+                Main programming language for your project.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="repository"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Description</FormLabel>
+              <FormControl>
+                <Input placeholder="Enter a repository URL" {...field} />
+              </FormControl>
+              <FormDescription>
+                Link to the github repository of your project.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Create</Button>
+      </form>
+    </Form>
+  );
+}
--- src/app/create-room/page.tsx
+++ src/app/create-room/page.tsx
@@ -1,7 +1,10 @@
+import { CreateRoomForm } from "./create-room-form";
+
 export default function CreateRoom() {
   return (
-    <main className="min-h-screen p-4">
+    <main className="flex min-h-screen flex-col items-center gap-4 p-4">
       <h1 className="text-3xl font-bold">Create Room</h1>
+      <CreateRoomForm />
     </main>
   );
 }

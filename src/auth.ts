d8ba34ad9f0e7155c942470b1a49c3ff99152d1e
Roman Timoshkin <rmntim@yandex.ru>
2024-05-03 13:31:09 +0300

    feat: add auth and vercel db

---
+++ src/auth.ts
@@ -0,0 +1,10 @@
+import NextAuth from "next-auth";
+import { db } from "./server/db";
+import GitHub from "next-auth/providers/github";
+import { DrizzleAdapter } from "@auth/drizzle-adapter";
+
+// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  adapter: DrizzleAdapter(db),
+  providers: [GitHub],
+});

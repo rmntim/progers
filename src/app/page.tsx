93bd26135116290b7342673cf35f1976d9d9f003
Roman Timoshkin <rmntim@yandex.ru>
2024-05-04 19:59:12 +0300

    fix: use nextjs Link for prefetching

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import { RoomCard } from "~/components/room-card";
 import { Button } from "~/components/ui/button";
 import { db } from "~/server/db";
 import { SearchBar } from "../components/search-bar";
+import Link from "next/link";
 
 export default async function HomePage() {
   const rooms = await db.query.room.findMany();
@@ -10,7 +11,9 @@ export default async function HomePage() {
     <main className="min-w-screen flex min-h-screen flex-col items-center gap-8 p-4 pt-8">
       <div className="flex w-full flex-row items-center justify-between gap-4">
         <h1 className="text-3xl font-bold">Find Rooms</h1>
-        <Button>Create Room</Button>
+        <Button asChild>
+          <Link href="/create-room">Create Room</Link>
+        </Button>
       </div>
       <SearchBar />
       <div className="container grid grid-flow-row grid-cols-1 gap-8 pt-8 md:grid-cols-2 lg:grid-cols-3">
--- src/components/top-nav.tsx
+++ src/components/top-nav.tsx
@@ -2,15 +2,16 @@ import { ModeToggle } from "~/components/mode-toggle";
 import { SignIn } from "~/components/sign-in";
 import { auth } from "~/server/auth";
 import { SignOut } from "~/components/sign-out";
+import Link from "next/link";
 
 export async function TopNav() {
   const session = await auth();
 
   return (
     <nav className="flex items-center justify-between p-4">
-      <a href="/" className="text-2xl font-bold">
+      <Link href="/" className="text-2xl font-bold">
         progers
-      </a>
+      </Link>
       <div className="flex items-center gap-4">
         {session === null ? (
           <SignIn />

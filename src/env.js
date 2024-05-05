1d7f93370cddcb1f96dc1b94bbc815d63f5c2744
Roman Timoshkin <rmntim@yandex.ru>
2024-05-05 11:06:14 +0300

    feat: add stream app secret

--- src/env.js
+++ src/env.js
@@ -11,6 +11,7 @@ export const env = createEnv({
     NODE_ENV: z
       .enum(["development", "test", "production"])
       .default("development"),
+    STREAM_APP_SECRET: z.string(),
   },
 
   /**
@@ -29,6 +30,7 @@ export const env = createEnv({
   runtimeEnv: {
     DATABASE_URL: process.env.POSTGRES_URL,
     NODE_ENV: process.env.NODE_ENV,
+    STREAM_APP_SECRET: process.env.STREAM_APP_SECRET,
     NEXT_PUBLIC_STREAM_API_KEY: process.env.NEXT_PUBLIC_STREAM_API_KEY,
   },
   /**

2edd643468b99c3a010803c756e6bec42dec9b25
Roman Timoshkin <rmntim@yandex.ru>
2024-05-05 11:52:06 +0300

    refactor: add paddings to room info

--- src/app/rooms/[roomId]/page.tsx
+++ src/app/rooms/[roomId]/page.tsx
@@ -24,14 +24,16 @@ export default async function Room({ params }: RoomProps) {
         </div>
         <div className="col-span-1 flex flex-col items-start justify-between gap-4 rounded-md border p-8">
           <div>
-            <h2 className="text-2xl font-bold">{room.name}</h2>
+            <h2 className="pb-4 text-2xl font-bold">{room.name}</h2>
             {room.repository && (
               <GithubLink
-                className="py-4 text-stone-400"
+                className="text-stone-400"
                 repository={room.repository}
               />
             )}
-            <p>{room.description ? room.description : "No description"}</p>
+            <p className="pt-4">
+              {room.description ? room.description : "No description"}
+            </p>
           </div>
           <p className="text-stone-400">Language: {room.language}</p>
         </div>

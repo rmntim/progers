acaf70d839debd5a1f5bdaf283ffb6ebb1bd464e
Roman Timoshkin <rmntim@yandex.ru>
2024-05-05 10:30:52 +0300

    refactor: change room page ui

--- src/app/rooms/[roomId]/page.tsx
+++ src/app/rooms/[roomId]/page.tsx
@@ -17,27 +17,22 @@ export default async function Room({ params }: RoomProps) {
 
   return (
     <main className="min-w-screen flex min-h-screen flex-col gap-4 p-4">
-      <div className="flex flex-row items-center justify-between gap-4">
-        <h1 className="pt-4 text-3xl font-bold">{room.name}</h1>
-        {room.repository && (
-          <GithubLink className="text-stone-400" repository={room.repository} />
-        )}
-      </div>
       <div className="grid grid-cols-4 gap-4">
-        <div className="col-span-3 flex aspect-video items-center justify-center rounded-md border">
+        <div className="col-span-3 flex items-center justify-center rounded-md border">
           VIDEO PLAYER
         </div>
-        <div className="col-span-1 flex items-center justify-center rounded-md border">
-          INFO PANEL
-        </div>
-      </div>
-      <div className="flex flex-row items-center justify-between p-4">
-        <div className="flex flex-col items-start justify-center gap-4">
-          <h2 className="text-2xl font-bold">Description</h2>
-          <p>{room.description ? room.description : "No description"}</p>
-        </div>
-        <div>
-          <h2 className="text-stone-400">{room.language}</h2>
+        <div className="col-span-1 flex flex-col items-start justify-between gap-4 rounded-md border p-8">
+          <div>
+            <h2 className="text-2xl font-bold">{room.name}</h2>
+            {room.repository && (
+              <GithubLink
+                className="py-4 text-stone-400"
+                repository={room.repository}
+              />
+            )}
+            <p>{room.description ? room.description : "No description"}</p>
+          </div>
+          <p className="text-stone-400">Language: {room.language}</p>
         </div>
       </div>
     </main>
